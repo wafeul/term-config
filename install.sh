6922e56d10e98f38ba92aa9f62a7e3b06cc84282
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-02-08 11:46:41 +0100

    first update
    first update about asking permission to do stuff from user

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
-# Copy .bashrc and save old one as ~/.bashrc.old
-mv ~/.bashrc ~/.bashrc.old
-cp ./.bashrc ~/.bashrc
+#!/bin/bash
 
-# 
+echo "This install is for Ubuntu related OS only."
+echo "If you have sudo rights please continue, otherwise ask your IT to install curl first."
+select yn in "Continue" "Exit"
+do
+    case $yn in
+        Continue ) break ;;
+        Exit ) exit ;;
+    esac
+done
+echo "We will now install xcfe terminal"
+select yn in "Install" "Continue"
+do
+    case $yn in
+        Install ) sudo apt install xfce4-terminal
+            # Add nord.theme to xfce local share folder
+            if [ ! -d ~/.local/share/xfce4/terminal/colorschemes ]
+            then
+                mkdir -p ~/.local/share/xfce4/terminal/colorschemes 
+            fi
+            cp ./nord.theme ~/.local/share/xfce4/terminal/colorschemes
+            break ;;
+        Continue ) break ;;
+    esac
+done
+
+echo "Do you wish to replace your .bashrc file with this one? If yes your old one will be stored in ~/.bashrc.old"
+select yn in "Yes" "No"
+do
+    case $yn in
+        Yes ) 
+            mv ~/.bashrc ~/.bashrc.old
+            cp ./.bashrc ~/.bashrc
+            break ;;
+        No ) break ;;
+    esac
+done
